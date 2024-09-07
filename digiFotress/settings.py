07af4a73b816149ace4d04812f33672d478c8cf8
Tej749 <jargha261975@gmail.com>
2024-09-07 16:10:17 +0545

    allowhost

--- digiFotress/settings.py
+++ digiFotress/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-$!y@+d*2-f$)l54k76=arjvenu2lv--_atz1z3&!9j@wb9dphh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
